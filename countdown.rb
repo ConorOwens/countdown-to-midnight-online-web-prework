a6625ae399d02980ed01bc196d24654e84de8b93
Conor Owens <conorj.owens@gmail.com>
2019-03-20 06:09:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,14 @@
 
 def countdown(n)
   while n !< 0
-  puts "#{n} SECOND(S)"
+  puts "#{n} SECOND(S)!"
   n -= 1
 "HAPPY NEW YEAR!"
 end
 end
+
+def countdown_with_sleep(n)
+  while n !< 0
+  puts "#{n} SECOND(S)!"
+  n -= 1
+  sleep(1)
